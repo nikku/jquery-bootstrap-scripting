c467370c52096d73da92421b2ac50549aa99c960
Nico Rehwaldt <git_nikku@nixis.de>
2011-09-05 21:05:13 +0200

    Added additional samples and updated script

--- script/jquery.dialog2.js
+++ script/jquery.dialog2.js
@@ -7,7 +7,7 @@
  * Licensed under the MIT license 
  * http://www.opensource.org/licenses/mit-license.php 
  * 
- * @version: 1.0 (04/09/2011)
+ * @version: 1.0.1 (05/09/2011)
  * 
  * @requires jQuery >= 1.4 
  * 
@@ -16,7 +16,7 @@
  * 
  * @requires bootstrap styles (twitter.github.com/bootstrap) to look nice
  * 
- * @author nico.rehwaldt (nire-nokin@gmail.com)
+ * @author nico.rehwaldt
  */
 (function($) {
     
@@ -100,44 +100,41 @@
     };
     
     /**
+     * Cached functions (to be memorized for some reason)
+     */
+    var __removeDialog = function(event) { $(this).remove(); };
+    
+    /**
      * Public api for dialog2
      */
     var dialog2 = {
         close: function() {
             var dialog = $(this);
+            var overlay = dialog.parents(".modal-overlay");
+            
+            overlay.hide();
             dialog
-                .trigger("dialog2-before-close")
-                .parents(".overlay")
-                    .hide()
-                    .end()
                 .trigger("dialog2-closed")
-                .parents(".overlay")
-                    .remove();
+                .removeClass("opened");
         }, 
         open: function() {
             var dialog = $(this);
             
-            if (!dialog.is(".opened")) {
+            if (!dialog.is(".opened")) {                
                 dialog
                     .trigger("dialog2-before-open")
-                    .parents(".overlay")
+                    .parents(".modal-overlay")
                         .show()
                         .end()
                     .addClass("opened")
                     .trigger("dialog2-opened");
             }
         }, 
-        addButton: function(name, options) {
-            var footer = $(this)
-                .parents(".modal")
-                .find(".modal-footer");
-            
-            addLightboxButton(footer, name, options);
+        addButton: function(name, options) {          
+            addDialogButton(footer, name, options);
         }, 
         removeButton: function(name) {
-            var footer = $(this)
-                .parents(".modal")
-                .find(".modal-footer");
+            var footer = $(this).parent().find(".modal-footer");
                 
             footer
                 .find("a.btn")
@@ -146,21 +143,17 @@
         }, 
         options: function(options) {
             var self = $(this);
+            var handle = self.parent();
             
-            if (options.autoOpen) {
-                self.dialog2("open");
-            }
-            
-            var handle = self.parents(".modal-overlay");
             if (options.title) {
                 $(".modal-header h3", handle).text(options.title);
             }
             
             if (options.buttons) {
-                var footer = $(".modal-footer", handle).empty();
+                $(".modal-footer", handle).empty();
                 
                 $.each(options.buttons, function(name, value) {
-                    addLightboxButton(footer, name, value);
+                    addDialogButton(self, name, value);
                 });
             }
             
@@ -173,16 +166,41 @@
                     }
                 });
             }
+            
+            
+            if (options.content) {
+                self.trigger("dialog2.before-load")
+                    .loadWithoutCache(options.content, loadComplete);
+            }
+            
+            self.unbind("dialog2-closed", __removeDialog);
+            
+            if (options.removeOnClose) {
+                self.bind("dialog2-closed", __removeDialog);
+            }
+            
+            if (options.autoOpen) {
+                self.dialog2("open");
+            }
         }
     };
     
-    function addLightboxButton(footer, name, options) {
+    /**************************************************************************
+     * Private utility methods                                                *
+     **************************************************************************/
+    
+    function addDialogButton(dialog, name, options) {
         var callback = $.isFunction(options) ? options : options.click;
         
+        var footer = $(dialog).parent().find(".modal-footer");
+
         var button = $("<a href='#' class='btn'></a>")
                             .text(name)
-                            .click(callback);
-                            
+                            .click(function(event) {
+                                callback.apply(dialog, [event]);
+                                event.preventDefault();
+                            });
+        
         if (options.primary) {
             button.addClass("primary");
         }
@@ -190,22 +208,20 @@
         footer.append(button);
     };
     
-    /**************************************************************************
-     * Private utility methods                                                *
-     **************************************************************************/
-    function checkCreateLightBox(element, options) {
-        var html = "<div class='overlay modal-overlay'>" + 
-"<div class='modal' style='position: relative; top: auto; left: auto; margin: 10% auto; z-index: 1'>" + 
-"<div class='modal-header'>" +
-"<h3></h3><span class='loader'></span>" + 
-"<a href='#' class='close'></a>" + 
-"</div>" + 
-"<div class='modal-body'>" + 
-"</div>" + 
-"<div class='modal-footer'>" + 
-"</div>" + 
-"</div>" + 
-"</div>";
+    var DIALOG_HTML = "<div class='overlay modal-overlay'>" + 
+        "<div class='modal' style='position: relative; top: auto; left: auto; margin: 10% auto; z-index: 1'>" + 
+        "<div class='modal-header'>" +
+        "<h3></h3><span class='loader'></span>" + 
+        "<a href='#' class='close'></a>" + 
+        "</div>" + 
+        "<div class='modal-body'>" + 
+        "</div>" + 
+        "<div class='modal-footer'>" + 
+        "</div>" + 
+        "</div>" + 
+        "</div>";
+    
+    function checkCreateDialog(element, options) {
         
         /**
          * // selector is a dialog? Does essentially nothing
@@ -225,50 +241,57 @@
         var dialog;
         
         if (!selection.is(".modal-body")) {
-            var overlay = $(html).appendTo("body");
+            var overlay = $(DIALOG_HTML).appendTo("body");
             
-            dialog = overlay
-                .find(".modal-header a.close")
-                    .click(function() {
-                        $(this)
-                            .parents(".modal")
-                            .find(".modal-body")
-                                .dialog2("close");
-                    })
-                    .text(unescape("%D7"))
-                    .end()
-                .find(".modal-body")
-                    .append(selection.contents());
-            
-            // Remove element the content originated from
-            selection.remove();
-            
-            dialog
-                .ajaxSend(function() {
+            $(".modal-header a.close", overlay)
+                .text(unescape("%D7"))
+                .click(function() {
                     $(this)
-                        .trigger("dialog2.before-load")
-                        .dialog2({ buttons: localizedCancelButton() })
-                        .parent()
-                            .addClass("loading"); 
-                })
-                .ajaxComplete(function() {
-                    $(this)
-                        .trigger("dialog2.load-complete")
-                        .parent()
-                            .removeClass("loading");
+                        .parents(".modal")
+                        .find(".modal-body")
+                            .dialog2("close");
                 });
+            
+            dialog = $(".modal-body", overlay);
+            if (!dialog.length) {
+                throw new Error("No dialog");
+            }
+            
+            // Create dialog body from current jquery selection
+            // If specified body is a div element and only one element is 
+            // specified, make it the new modal dialog body
+            // Allows us to do something like this 
+            // $('<div id="foo"></div>').dialog2(); $("#foo").dialog2("open");
+            if (selection.is("div") && selection.length == 1) {
+                dialog.replaceWith(selection);
+                selection.addClass("modal-body");
+                dialog = selection;
+            }
+            // If not, append current selection to dialog body
+            else {
+                dialog.append(selection);
+            }
+            
+            dialog.bind("dialog2.before-load", function() {
+                $(this).dialog2({ buttons: localizedCancelButton() })
+                       .parent().addClass("loading");
+            });
+            
+            dialog.bind("dialog2.load-complete", function() {
+                $(this).parent().removeClass("loading")
+            });
             
             if (options.id) {
                 dialog.attr("id", options.id);
             }
-            
         } else {
             dialog = selection;
         }
-
-        if (options.content) {
-            dialog.loadWithoutCache(options.content, loadComplete);
+        
+        if (!dialog.is(".modal-body")) {
+            throw new Error("Dialog is not a modal dialogs body");
         }
+        
         return dialog;
     };
     
@@ -297,11 +320,14 @@
     function loadComplete() {
         var dialog = $(this);
         
+        dialog.trigger("dialog2.load-complete");
+        
         dialog.find("a.ajax").click(function(event) {
             var url = $(this).attr("href");
             
             event.preventDefault();
             dialog
+                .trigger("dialog2.before-load")
                 .loadWithoutCache(url, function(data, status, request) {
                     $(dialog).html(data);
                     loadComplete.call(dialog, data, status, request);
@@ -313,7 +339,10 @@
             // if submitable form is found
             var form = $("form.ajax", dialog).ajaxForm({
                 target: dialog,
-                success: loadComplete, 
+                success: loadComplete,
+                beforeSubmit: function() {
+                    dialog.trigger("dialog2.before-load");
+                }, 
                 cache: false
             });
 
@@ -362,31 +391,32 @@
          * 
          * $(".selector").dialog2("method", arguments);
          */
-        dialog2: function() {
-            
+        dialog2: function() {            
             var args = $.makeArray(arguments);
             
             var arg0 = args.shift();
             if (dialog2[arg0]) {
                 return dialog2[arg0].apply(this, args);
+            } else {
+                var options = {};
+
+                if ($.isPlainObject(arg0)) {
+                    options = $.extend(true, {}, $.fn.dialog2.defaults, arg0);
+                }            
+                
+                var dialog = checkCreateDialog(this, options);
+                
+                $(dialog).dialog2("options", options);
+                
+                return dialog;
             }
-            
-            var options = {};
-            
-            if ($.isPlainObject(arg0)) {
-                options = $.extend(true, {}, $.fn.dialog2.defaults, arg0);
-            }
-            
-            return this.each(function() {
-                var e = checkCreateLightBox(this, options);
-                $(e).dialog2("options", options);
-            });
         }
     });
     
     $.fn.dialog2.defaults = {
         autoOpen: true, 
-        closeOnOverlayClick: true
+        closeOnOverlayClick: true, 
+        removeOnClose: true
     };
     
     $.fn.dialog2.localization = {
@@ -418,28 +448,27 @@
     if ($.fn.controls && $.fn.controls.bindings) {
         $.extend($.fn.controls.bindings, {
             "a.ajax": function() {
-                var a = $(this);
-                var dialog = $("<div></div>");
-                
-                var options = {
-                    modal: true,
-                    content: a.attr("href"),
-                    id: a.attr("rel"),
+                $(this).click(function(event) {
+                    var a = $(this);
                     
-                    buttons: localizedCancelButton()
-                };
-                
-                if (a.hasClass("open-lazy")) {
-                    options.autoOpen = false;
-                }
-                
-                if (a.attr("title")) {
-                    options.title = a.attr("title");
-                }
+                    var options = {
+                        modal: true,
+                        content: a.attr("href"),
+                        id: a.attr("rel"),
+                        
+                        buttons: localizedCancelButton()
+                    };
 
-                a.click(function(event) {
+                    if (a.hasClass("open-lazy")) {
+                        options.autoOpen = false;
+                    }
+
+                    if (a.attr("title")) {
+                        options.title = a.attr("title");
+                    }
+                    
+                    $("<div></div>").dialog2(options);
                     event.preventDefault();
-                    dialog.dialog2(options);
                 });
             }
         });
